8b7b980c6b5303ec715532a3a5203ca99f1d5141
piligab <piligab.psq@gmail.com>
2023-06-26 15:39:48 -0500

    update8

--- src/App.js
+++ src/App.js
@@ -10,15 +10,32 @@ const defaultTodos = [
   {text: 'Tomar el Curso de Intro a React.js', completed:false},
   {text: 'Llorar con la llorona', completed:false},
   {text: 'otro', completed:false},
+  {text: 'usar estados derivados', completed:true},
   
 ];
 
 function App() {
+  // estado del todo
+  const [todos, setTodos] = React.useState(defaultTodos);
+  // estado del todoSearch
+  const [searchValue, setSearchValue] = React.useState('');
+  console.log('Los usuarios buscan todos de ' + searchValue);
+
+  // estados derivados
+  // porqué aquí el nombre cambia todos - todo?
+  const completedTodos = todos.filter(
+      // doble negación va convertir en booleano cualquier cosa que estamos devolviendo
+      todo => !!todo.completed 
+      ).length;
+  const totalTodos = todos.length;
   return (
     <React.Fragment>
 
-      <TodoCounter completed={16} total={25}/>
-      <TodoSearch />
+      <TodoCounter completed={completedTodos} total={totalTodos}/>
+      <TodoSearch 
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />
 
       <TodoList>
         {defaultTodos.map(todo => (
