5682b98823acb77568c3f8479d677a513aa62b25
piligab <piligab.psq@gmail.com>
2023-06-27 15:28:45 -0500

    update11

---
+++ src/DeleteIcon.js
@@ -0,0 +1,12 @@
+import react from "react";
+import { TodoIcon } from './TodoIcon';
+
+function DeleteIcon(){
+    return (
+        <TodoIcon
+            type="delete"
+            color="red"
+        />
+    );
+}
+export { DeleteIcon };
